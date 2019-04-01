4d58809ddf63ad355f7ae82ce81eefe2da6f504e
Christian <jcromerohdz@gmail.com>
2019-04-01 10:34:28 -0700

    Some basic understandig of classes and objects

---
+++ OOPLessonOne/src/DogTestDrive.java
@@ -0,0 +1,10 @@
+public class DogTestDrive {
+    public static void  main(String[] args){
+        // Dog test code goes here
+
+        Dog d = new Dog();
+        d.size = 40;
+        d.bark();
+
+    }
+}
